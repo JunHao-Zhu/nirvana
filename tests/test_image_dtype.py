cf87ba222a92a2d7606e8dd9a430c0a96e129587
Junhao Zhu <zhujunhao@zju.edu.cn>
2025-03-30 17:00:12 +0800

    [test] support image data

--- tests/test_image_dtype.py
+++ tests/test_image_dtype.py
@@ -2,5 +2,22 @@ import pandas as pd
 import mahjong as mjg
 
 
-def test_image_type():
-    pd.ImageDtype
+def test_image_data():
+    mjg.configure_llm_backbone(
+        model_name="Deepseek-chat",
+        api_key="<Your-API-Key>",
+        base_url="https://api.deepseek.com"
+    )
+
+    logo_imgs = mjg.ImageArray([
+        "https://spark.apache.org/images/spark-logo.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/PyTorch_logo_black.svg/488px-PyTorch_logo_black.svg.png",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Databricks_Logo.png/960px-Databricks_Logo.png"
+    ])
+    df = pd.DataFrame({
+        "names": ["Spark", "Pytorch", "Databricks"],
+        "logos": logo_imgs
+    })
+    
+    outputs = mjg.ops.filter(df["logos"], "Whether the logo is about a software or not?", strategy="plain_llm")
+    return outputs
