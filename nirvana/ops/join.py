8413abf1f6aa2ca6c9c73b467498fa664774beeb
Junhao Zhu <zhujunhao@zju.edu.cn>
2025-09-24 19:05:56 +0800

    Modify the execution and outputs of join

--- nirvana/ops/join.py
+++ nirvana/ops/join.py
@@ -1,4 +1,5 @@
 import asyncio
+from pydantic import BaseModel
 import pandas as pd
 from typing import Any, List, Iterable, Tuple
 from dataclasses import dataclass
@@ -12,8 +13,9 @@ def join_wrapper(
     left_data: pd.DataFrame,
     right_data: pd.DataFrame, 
     user_instruction: str,
-    left_column: str,
-    right_column: str, 
+    left_on: str,
+    right_on: str,
+    how: str = "inner",
     **kwargs
 ):
     join_op = JoinOperation()
@@ -21,9 +23,9 @@ def join_wrapper(
         left_data=left_data,
         right_data=right_data,
         user_instruction=user_instruction,
-        left_column=left_column,
-        right_column=right_column,
-        strategy=None,
+        left_on=left_on,
+        right_on=right_on,
+        how=how,
         **kwargs
     )
     return outputs
@@ -31,8 +33,9 @@ def join_wrapper(
 
 @dataclass
 class JoinOpOutputs(BaseOpOutputs):
-    output: Iterable[bool] = None
     joined_pairs: List[tuple] = None
+    left_join_keys: List[int] = None
+    right_join_keys: List[int] = None
 
 
 class JoinOperation(BaseOperation):
@@ -59,7 +62,14 @@ class JoinOperation(BaseOperation):
             output, total_cost = output["output"], output["cost"]
             return output, total_cost
     
-    def _postprocess_join_outputs(self, data_id_pairs: List[tuple], results: Iterable[Tuple[Any, float]]):
+    def _postprocess_join_outputs(
+            self, 
+            data_id_pairs: List[tuple], 
+            results: Iterable[Tuple[Any, float]],
+            how: str = "inner",
+            left_ids: List[int] = None,
+            right_ids: List[int] = None
+    ):
         join_outputs, costs = [], 0.0
         for output, cost in results:
             if output is None:
@@ -75,48 +85,57 @@ class JoinOperation(BaseOperation):
             costs += cost
 
         joined_pairs = []
+        left_keys, right_keys = left_ids, right_ids
         for i, output in enumerate(join_outputs):
             if output:
+                if how == "inner" or how == "left":
+                    right_keys[data_id_pairs[i][1]] = left_keys[data_id_pairs[i][0]]
+                else:
+                    left_keys[data_id_pairs[i][0]] = right_keys[data_id_pairs[i][1]]
                 joined_pairs.append(data_id_pairs[i])
 
-        return joined_pairs, join_outputs, costs
+        return joined_pairs, left_keys, right_keys, costs
 
     async def execute(
             self, 
             left_data: pd.DataFrame,
             right_data: pd.DataFrame,
             user_instruction: str,
-            left_column: str,
-            right_column: str,
-            how: str = "left",
-            strategy: str = None,
+            left_on: str,
+            right_on: str,
+            how: str = "inner",
             *args, 
             **kwargs
     ):
-        assert left_data[left_column].dtype == right_data[right_column].dtype, (
+        assert left_data[left_on].dtype == right_data[right_on].dtype, (
             "Data types of columns to join must be the same."
         )
-        if isinstance(left_data[left_column].dtype, ImageDtype):
+        if isinstance(left_data[left_on].dtype, ImageDtype):
             dtype = "image"
         else:
             dtype = "str"
         
-        left_ids = left_data[left_column].index
-        right_ids = right_data[right_column].index
+        left_ids = left_data[left_on].index
+        right_ids = right_data[right_on].index
         data_id_pairs = [(left_id, right_id) for left_id in left_ids for right_id in right_ids]
 
         tasks = []
         for left_id, right_id in data_id_pairs:
-            left_value = left_data.loc[left_id, left_column]
-            right_value = right_data.loc[right_id, right_column]
+            left_value = left_data.loc[left_id, left_on]
+            right_value = right_data.loc[right_id, right_on]
             tasks.append(asyncio.create_task(self._plain_llm_execute(left_value, right_value, user_instruction, dtype)))
         
         # Wait for all tasks to complete
         results = await asyncio.gather(*tasks)
 
-        joined_pairs, join_outputs, token_cost = self._postprocess_join_outputs(data_id_pairs, results)
+        joined_pairs, left_join_keys, right_join_keys, token_cost = (
+            self._postprocess_join_outputs(
+                data_id_pairs, results, how, list(range(len(left_ids))), list(range(len(right_ids)))
+            )
+        )
         return JoinOpOutputs(
-            output=join_outputs,
-            joined_pairs=joined_pairs,
+            output=joined_pairs,
+            left_join_keys=left_join_keys,
+            right_join_keys=right_join_keys,
             cost=token_cost,
         )
