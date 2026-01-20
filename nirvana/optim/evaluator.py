6c11254375aa4e39fea18277369e1280f281fe25
Junhao Zhu <zhujunhao@zju.edu.cn>
2026-01-20 11:22:02 +0800

    Remove unnecessary files

--- nirvana/optim/evaluator.py
+++ nirvana/optim/evaluator.py
@@ -1,8 +1,6 @@
-import asyncio
 import pandas as pd
 
 from nirvana.executors.llm_backbone import LLMClient
-from nirvana.optim.optimize_prompt import RESULT_EVALUATE_PROMPT
 
 
 def table_serialize(df: pd.DataFrame) -> str:
@@ -12,21 +10,30 @@ def table_serialize(df: pd.DataFrame) -> str:
 
 
 class Evaluator:
-    @staticmethod
-    async def evaluate(ground_truth: pd.DataFrame, process_result: pd.DataFrame, evaluator: LLMClient):
+    evaluate_prompt = """Here are a golden analysis result obtained by a golden data processing plan and a result derived from an alternative data processing plan.
+Evaluate the two data analysis results and return a rating between 0 and 10, where 0 means the two results are completely different and 10 means they are exactly the same.
+
+Ground truth:
+{ground_truth}
+
+Result from the alternative plan:
+{result}
+
+You should carefully consider all values (and their semantics) in both analysis results. The rating score is enclosed within <score></score> tags, i.e., <score>Rating Score</score>.
+"""
+
+    @classmethod
+    async def evaluate(cls, ground_truth: pd.DataFrame, process_result: pd.DataFrame, evaluator: LLMClient):
         if not ground_truth.columns.equals(process_result.columns):
             return 0.0
         
         if ground_truth.size != process_result.size:
             return 0.0
         
-        evaluate_prompt = [{
-            "role": "user",
-            "content": RESULT_EVALUATE_PROMPT.format(
-                ground_truth=table_serialize(ground_truth),
-                result=table_serialize(process_result)
-            )
-        }]
+        evaluate_prompt = cls.evaluate_prompt.format(
+            ground_truth=table_serialize(ground_truth),
+            result=table_serialize(process_result)
+        )
         response = await evaluator(messages=evaluate_prompt, parse_tags=True, tags=["score"])
         rating = float(response["score"]) / 10.0
         return rating
